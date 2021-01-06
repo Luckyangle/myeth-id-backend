3166442e009b11a7ae1eec62f869113756388e08
taosue <first@taosue.xyz>
2021-01-06 15:28:39 +0800

    add respond message

--- src/respond.ts
+++ src/respond.ts
@@ -1,9 +1,11 @@
 class Respond {
   public data: any;
   public statusCode: number;
-  constructor(data: any, statusCode: number) {
+  public message: string;
+  constructor(data: any, statusCode: number, message: string) {
     this.data = data;
     this.statusCode = statusCode;
+    this.message = message;
   }
 }
 export default Respond;
